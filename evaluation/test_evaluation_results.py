b88ebfab47ded1963b5406f747742cabdbc7c196
lwaekfjlk <1125027232@qq.com>
2024-06-20 23:36:44 -0400

    update gitignore

--- evaluation/test_evaluation_results.py
+++ evaluation/test_evaluation_results.py
@@ -1,6 +1,14 @@
 import jsonlines
 
-exp_names = ['claude-2_maxiter_50_N_v1.3', 'gemini-1.5-flash_maxiter_50_N_v1.3']
+exp_names = [
+    'claude-2_maxiter_50_N_v1.3',
+    'gemini-1.5-flash_maxiter_50_N_v1.3',
+    'gpt-4o_maxiter_50_N_v1.3',
+    'gpt-4-turbo_maxiter_50_N_v1.3',
+    'gpt-3.5-turbo_maxiter_50_N_v1.3',
+    'Mixtral-8x22B-Instruct-v0.1_maxiter_50_N_v1.3',
+    'Qwen2-72B-Instruct_maxiter_50_N_v1.3',
+]
 
 for exp_name in exp_names:
     with jsonlines.open(
@@ -13,11 +21,20 @@ for exp_name in exp_names:
     applied = 0
     resolved = 0
 
+    jsonline_data = []
     for data in dataset:
+        if 'resolved' in data['test_result']['result'].keys():
+            jsonline_data.append(data)
+        else:
+            continue
         if 'test_result' in data and 'result' in data['test_result']:
             resolved += 1 if data['test_result']['result']['resolved'] > 0 else 0
         if 'git_patch' in data:
             generated += 1 if len(data['git_patch']) > 0 else 0
+
+    with jsonlines.open(f'./{exp_name}_valid.jsonl', 'w') as f:
+        for data in jsonline_data:
+            f.write(data)
 
     print(exp_name)
     print(f'Generated: {generated}')
