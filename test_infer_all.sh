6514ed581af013deff47d4df03a4a29da54e6c52
lwaekfjlk <1125027232@qq.com>
2024-06-21 13:50:49 -0400

    add a bash for test infer

---
+++ test_infer_all.sh
@@ -0,0 +1,2 @@
+./evaluation/swe_bench/scripts/eval_infer.sh evaluation/evaluation_outputs/outputs/swe_bench/CodeActAgent/gpt-4-turbo_maxiter_50_N_v1.3/output.jsonl;
+./evaluation/swe_bench/scripts/eval_infer.sh evaluation/evaluation_outputs/outputs/swe_bench/CodeActAgent/Qwen2-72B-Instruct_maxiter_50_N_v1.3/output.jsonl;
