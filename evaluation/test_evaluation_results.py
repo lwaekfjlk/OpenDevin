5e75f7e108444b4891efaa349c1194d18b6f0ab3
lwaekfjlk <1125027232@qq.com>
2024-07-08 14:01:33 -0400

    add test eval limited to dev

--- evaluation/test_evaluation_results.py
+++ evaluation/test_evaluation_results.py
@@ -52,9 +52,12 @@ exp_names = [
     #'Qwen2-72B-Instruct_maxiter_50_N_v1.3',
     #'gemini-1.5-pro_maxiter_50_N_v1.3', 
     #'gemini-1.5-pro-latest_maxiter_50_N_v1.3',
-    'claude-3-opus-20240229_maxiter_50_N_v1.3',
+    #'claude-3-opus-20240229_maxiter_50_N_v1.3',
     #'claude-3-5-sonnet-20240620_maxiter_50_N_v1.3',
+    'Codestral-22B-v0.1_maxiter_50_N_v1.3'
 ]
+
+dev_instance_ids = ['django__django-10914', 'django__django-11099', 'django__django-14382', 'django__django-14580', 'django__django-15789', 'django__django-16527', 'matplotlib__matplotlib-23964', 'matplotlib__matplotlib-24334', 'mwaskom__seaborn-3010', 'psf__requests-863', 'pytest-dev__pytest-5227', 'pytest-dev__pytest-5413', 'pytest-dev__pytest-7168', 'sympy__sympy-13480', 'django__django-13964', 'django__django-14915', 'matplotlib__matplotlib-24149', 'pytest-dev__pytest-11143', 'scikit-learn__scikit-learn-13142', 'sphinx-doc__sphinx-8713', 'sympy__sympy-13647', 'sympy__sympy-20590', 'sympy__sympy-23117', 'sympy__sympy-24213']
 
 MAX_ITER = 50
 
@@ -75,6 +78,8 @@ def process_experiment_files(exp_names, file_suffix, max_iter):
         oracle_data = []
 
         for data in dataset:
+            if data['instance_id'] not in dev_instance_ids:
+                continue
             oracle_datapoint = data.copy()
             oracle_datapoint['git_patch'] = oracle_datapoint['swe_instance']['patch']
             oracle_data.append(oracle_datapoint)
